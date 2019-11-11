bfe5325cf040c52ac6b02bf4c8d252aefa79531a
JieChen <895066953@qq.com>
2019-11-11 16:25:41 +0800

    更新完element-ui后的版本

--- src/views/account/list.vue
+++ src/views/account/list.vue
@@ -21,51 +21,61 @@
           <el-button type="primary" @click="filterSubmit">查询</el-button>
         </el-form-item>
       </el-form>
-      <el-table :data="tableData4" style="width: 100%" max-height="600">
-  <el-table-column type="expand">
+      <el-table :data="tableData" size="mini" ref="filterTable" :max-height="tableHeight" style="width: 100%">
+        <el-table-column type="expand">
           <template slot-scope="props">
-            <el-form inline class="demo-table-expand">
+            <el-form label-position="left" inline class="demo-table-expand">
               <el-form-item label="商品名称">
-                <span>{{ props.row.date }}</span>
-              </el-form-item>
-              <el-form-item label="所属店铺">
                 <span>{{ props.row.name }}</span>
               </el-form-item>
+              <el-form-item label="所属店铺">
+                <span>{{ props.row.shop }}</span>
+              </el-form-item>
               <el-form-item label="商品 ID">
-                <span>{{ props.row.province }}</span>
+                <span>{{ props.row.id }}</span>
               </el-form-item>
               <el-form-item label="店铺 ID">
-                <span>{{ props.row.city }}</span>
+                <span>{{ props.row.shopId }}</span>
               </el-form-item>
               <el-form-item label="商品分类">
-                <span>{{ props.row.address }}</span>
+                <span>{{ props.row.category }}</span>
               </el-form-item>
               <el-form-item label="店铺地址">
-                <span>{{ props.row.zip }}</span>
+                <span>{{ props.row.address }}</span>
+              </el-form-item>
+              <el-form-item label="商品描述">
+                <span>{{ props.row.desc }}</span>
               </el-form-item>
             </el-form>
           </template>
         </el-table-column>
-    <el-table-column prop="date" label="日期" width="150">
-    </el-table-column>
-    <el-table-column prop="name" label="姓名" width="120">
-    </el-table-column>
-    <el-table-column prop="province" label="省份" width="120">
-    </el-table-column>
-    <el-table-column prop="city" label="市区" width="120">
-    </el-table-column>
-    <el-table-column prop="address" label="地址" width="300">
-    </el-table-column>
-    <el-table-column prop="zip" label="邮编" width="120">
-    </el-table-column>
-    <el-table-column label="操作" width="120">
-      <template slot-scope="scope">
-        <el-button @click.native.prevent="deleteRow(scope.$index, tableData4)" type="text" size="small">
-          移除
-        </el-button>
-      </template>
-    </el-table-column>
-  </el-table>
+        <el-table-column
+          label="用户 ID"
+          prop="id">
+        </el-table-column>
+        <el-table-column
+          label="用户名"
+          prop="name">
+        </el-table-column>
+        <el-table-column
+          label="手机号"
+          prop="phone">
+        </el-table-column>
+        <el-table-column label="注册日期" prop="date" sortable column-key="date"
+        :filters="[{text: '2016-05-01', value: '2016-05-01'}, {text: '2016-05-02', value: '2016-05-02'}, {text: '2016-05-03', value: '2016-05-03'}, {text: '2016-05-04', value: '2016-05-04'}]"
+        :filter-method="filterHandler" width="180">
+          <template slot-scope="scope">
+            <i class="el-icon-time"></i>
+            <span style="margin-left: 10px">{{ scope.row.date }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作">
+          <template slot-scope="scope">
+            <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+            <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
       <div class="page-wrap">
         <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="currentPage" :page-size="20" background layout="total, prev, pager, next, jumper" :total="1000"></el-pagination>
       </div>
@@ -111,21 +121,36 @@ export default {
         shopId: '10333',
         date: '2019-05-02',
         phone: '18362652542'
-      }],
-      tableData4: [{
-        date: '2016-05-03',
-        name: '王小虎',
-        province: '上海',
-        city: '普陀区',
-        address: '上海市普陀区金沙江路 1518 弄',
-        zip: 200333
       }, {
-        date: '2016-05-02',
-        name: '王小虎',
-        province: '上海',
-        city: '普陀区',
-        address: '上海市普陀区金沙江路 1518 弄',
-        zip: 200333
+        id: '12987123',
+        name: '好滋好味鸡蛋仔',
+        category: '江浙小吃、小吃零食',
+        desc: '荷兰优质淡奶，奶香浓而不腻',
+        address: '上海市普陀区真北路',
+        shop: '王小虎夫妻店',
+        shopId: '10333',
+        date: '2019-05-02',
+        phone: '18362652542'
+      }, {
+        id: '12987123',
+        name: '好滋好味鸡蛋仔',
+        category: '江浙小吃、小吃零食',
+        desc: '荷兰优质淡奶，奶香浓而不腻',
+        address: '上海市普陀区真北路',
+        shop: '王小虎夫妻店',
+        shopId: '10333',
+        date: '2019-05-02',
+        phone: '18362652542'
+      }, {
+        id: '12987123',
+        name: '好滋好味鸡蛋仔',
+        category: '江浙小吃、小吃零食',
+        desc: '荷兰优质淡奶，奶香浓而不腻',
+        address: '上海市普陀区真北路',
+        shop: '王小虎夫妻店',
+        shopId: '10333',
+        date: '2019-05-02',
+        phone: '18362652542'
       }],
       filterArticle: {
         name: '',
